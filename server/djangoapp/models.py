3b04f97c215ec171cd1621511a5261a512af2034
Brandie Mallard <3xtr3m3g33k@gmail.com>
2025-06-15 13:09:35 -0400

    Module 3.2: Django models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,61 @@
-# Uncomment the following imports before adding the Model code
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True)
+    website = models.URLField(blank=True)
 
+    def __str__(self):
+        return self.name
 
-# Create your models here.
+class CarModel(models.Model):
+    ENGINE_TYPES = [
+        ('GAS', 'Gas'),
+        ('DIESEL', 'Diesel'),
+        ('ELECTRIC', 'Electric'),
+        ('HYBRID', 'Hybrid')
+    ]
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('MICRO', 'Micro'),
+        ('HATCHBACK', 'Hatchback'),
+        ('CROSSOVER', 'Crossover'),
+        ('COUPE', 'Coupe'),
+        ('COUPE SUV', 'Coupe SUV'),
+        ('OFF-ROADER', 'Off-Roader'),
+        ('PICK-UP', 'Pick-Up'),
+        ('MPV', 'MPV'),
+        ('VAN', 'Van'),
+        ('CAMPER', 'Camper'),
+        ('MINIVAN', 'Minivan'),
+        ('SPORT', 'Sport'),
+        ('CABRIOLET', 'Cabriolet'),
+        ('ROADSTER', 'Roadster'),
+        ('HYPER', 'Hyper'),
+        ('MUSCLE', 'Muscle'),
+        ('LIMOUSINE', 'Limousine'),
+        ('OPEN WHEEL', 'Open Wheel')
+    ]
+    
+    TRANSMISSION_TYPES = [
+        ('AUTO', 'Automatic'),
+        ('MANUAL', 'Manual')
+    ]
+    
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=20, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(default=2025, validators=[MaxValueValidator(2025), MinValueValidator(2015)])
+    engine_type = models.CharField(max_length=10, choices=ENGINE_TYPES, default='GAS')
+    transmission = models.CharField(max_length=10, choices=TRANSMISSION_TYPES, default='AUTO')
+    color = models.CharField(max_length=20, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return self.name
